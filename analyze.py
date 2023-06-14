97960b97b0575d34d8eb7ed7ff141d0e6853aa77
shellcodesniper <shellcodesniper@icloud.com>
2023-06-15 00:54:56 +0900

    Fix: Patch

--- analyze.py
+++ analyze.py
@@ -1,6 +1,7 @@
 if __name__ != "__main__":
   raise Exception("This script is not meant to be imported.")
 
+import dash, webbrowser, tqdm
 from kuuwange.loaders import Loader
 
 import numpy as np
@@ -14,7 +15,8 @@ import plotly.graph_objects as go
 import plotly.subplots as ms
 import plotly.express as px
 import datetime, os, sys, time, glob
-import dash, webbrowser
+import dash.html as html
+from dash.dash import dcc
 import dash_mantine_components as dmc
 
 
@@ -27,54 +29,95 @@ transact: pd.DataFrame = Loader.get_transactions()
 holidays: pd.DataFrame = Loader.get_holidays()
 
 plot_data = train_data.groupby(['date']).agg({'sales': 'sum'}).reset_index()
-plot_data['date'] = pd.to_datetime(plot_data['date'].astype(str), format='%Y-%m-%d')
+# plot_data['date'] = pd.to_datetime(plot_data['date'].astype(str), format='%Y-%m-%d')
 
 plot_data = plot_data.merge(oil_info, on='date', how='left')
 plot_data = plot_data.merge(transact, on='date', how='left')
 plot_data = plot_data.merge(store_info, on='store_nbr', how='left')
+plot_data = plot_data.merge(holidays, on='date', how='outer')
 
-fig = ms.make_subplots(rows=2, cols=2, subplot_titles=("Total Sales", "Oil Price", "Transactions", "Holidays"))
+cfig = ms.make_subplots(rows=2, cols=2, subplot_titles=("Total Sales", "Oil Price", "Transactions", "Holidays"))
 
-fig.add_trace(
+cfig.add_trace(
   go.Scatter(x=plot_data['date'], y=plot_data['sales'], name='Total Sales'),
   row=1, col=1
 )
-fig.add_trace(
-  go.Scatter(x=plot_data['date'], y=plot_data['dcoilwtico'], name='Oil Price'),
+cfig.add_trace(
+  go.Scatter(
+    x=plot_data['date'], y=plot_data['dcoilwtico'],
+    name='Oil Price',
+    line=dict(color='brown', width=2, smoothing=1.3)
+  ),
   row=1, col=2
 )
 
-fig.add_trace(
+cfig.add_trace(
   go.Scatter(x=plot_data['date'], y=plot_data['transactions'], name='Transactions'),
   row=2, col=1
 )
 
-fig.update_layout(height=600, width=800, title_text="Total Sales vs Oil Price vs Transactions vs Holidays")
+cfig.update_layout(height=800, width=1920, title_text="Total Sales vs Oil Price vs Transactions vs Holidays")
 
-fig.show()
+# NOTE : Building Datas
 
-mfig = ms.make_subplots(rows=4, cols=1, specs=[[{'rowspan':3}],[None],[None], [{'rowspan': 1}]], shared_xaxes=True, horizontal_spacing=0.03, vertical_spacing=0.01)
+md = Loader.get_merged(True) # TYPE : Merged Data
 
-# NOTE : Base Data
-mfig.add_trace(
-  go.Scatter(x=plot_data['date'], y=plot_data['sales'], name='Total Sales'),
-  row=1, col=1
-)
+md = md.groupby(['date'])
+print (md.head())
 
-# NOTE : Transaction Data
-mfig.add_trace(
-  go.Scatter(x=plot_data['date'], y=plot_data['transactions'], name='Transactions'),
-  row=4, col=1
-)
 
-mfig.update_layout(autosize=True, xaxis1_rangeslider_visible=False, xaxis2_rangeslider_visible=False, margin=dict(l=50, r=50, t=50, b=50), template='seaborn')
-  # dfig.update_layout(xaxis=dict(type="category", tickformat="%H%M"), xaxis2=dict(type="category", tickformat="%H%M"))
-mfig.update_xaxes(tickformat='%Y.%m.%d', zeroline=True, zerolinewidth=1, zerolinecolor='black', showgrid=True, gridwidth=1, gridcolor='lightgray', showline=True, linewidth=1, linecolor='black', mirror=True)
-mfig.update_yaxes(tickformat=',d', zeroline=True, zerolinewidth=1, zerolinecolor='black', showgrid=True, gridwidth=2, gridcolor='lightgray',showline=True,linewidth=2, linecolor='black', mirror=True)
-mfig.update_traces(xhoverformat='%Y.%m.%d') 
-mfig.update_layout(width=1920, height=800)
-mfig.update_layout(paper_bgcolor='white', plot_bgcolor='white')
-mfig.update_layout(hovermode="x unified")
+# mfig = ms.make_subplots(rows=4, cols=1, specs=[[{'rowspan':3}],[None],[None], [{'rowspan': 1}]], shared_xaxes=True, horizontal_spacing=0.03, vertical_spacing=0.01)
+#
+# # NOTE : Base Data
+# mfig.add_trace(
+#   go.Scatter(x=md['date'], y=md['sales'], name='Total Sales'),
+#   row=1, col=1
+# )
+#
+# # NOTE : Transaction Data
+# mfig.add_trace(
+#   go.Scatter(x=md['date'], y=md['transactions'], name='Transactions'),
+#   row=4, col=1
+# )
+#
+# mfig.update_layout(autosize=True, xaxis1_rangeslider_visible=False, xaxis2_rangeslider_visible=False, margin=dict(l=50, r=50, t=50, b=50), template='seaborn')
+#   # dfig.update_layout(xaxis=dict(type="category", tickformat="%H%M"), xaxis2=dict(type="category", tickformat="%H%M"))
+# mfig.update_xaxes(tickformat='%Y.%m.%d', zeroline=True, zerolinewidth=1, zerolinecolor='black', showgrid=True, gridwidth=1, gridcolor='lightgray', showline=True, linewidth=1, linecolor='black', mirror=True)
+# mfig.update_yaxes(tickformat=',d', zeroline=True, zerolinewidth=1, zerolinecolor='black', showgrid=True, gridwidth=2, gridcolor='lightgray',showline=True,linewidth=2, linecolor='black', mirror=True)
+# mfig.update_traces(xhoverformat='%Y.%m.%d') 
+# mfig.update_layout(width=1920, height=800)
+# mfig.update_layout(paper_bgcolor='white', plot_bgcolor='white')
+# mfig.update_layout(hovermode="x unified")
 
-mfig.show()
+# print (plot_data)
 
+# Add Holiday Data
+# mfig.add_trace(
+#   go.Scatter(x=plot_data['date'], y=plot_data['type'], name='Holidays', mode='markers', marker=dict(color='red', size=10)),
+# )
+
+
+app = dash.Dash(__name__)
+app.css.config.serve_locally = True
+app.scripts.config.serve_locally = True
+app.layout = html.Div(children=[
+  html.Div(children=[
+    html.H3(children='분석용 도구'),
+    html.Br(),
+    html.Div(children=[
+      html.Div(children=[
+        html.H4(children='전체 매출'),
+        dcc.Graph(id='total-sales', figure=cfig)
+      ], className='six columns'),
+      html.Div(children=[
+        html.H4(children='매장별 매출'),
+        # dcc.Graph(id='store-sales', figure=mfig)
+      ], className='six columns')
+    ], className='row')
+  ], className='container')
+])
+
+
+if __name__ == "__main__":
+  # webbrowser.open('http://localhost:2000')
+  app.run_server(debug=True, port=2000)
--- kuuwange/loaders.py
+++ kuuwange/loaders.py
@@ -21,8 +21,9 @@ class _Loaders:
       return
     self.train_raw = pd.read_csv(
     os.path.join(BASE_PATH, 'train.csv'),
-      usecols=['store_nbr', 'family', 'date', 'sales', 'onpromotion'],
+      usecols=['id','store_nbr', 'family', 'date', 'sales', 'onpromotion'],
       dtype={
+        'id': 'uint32',
         'store_nbr': 'category',
         'family': 'category',
         'sales': 'float32',
@@ -35,7 +36,9 @@ class _Loaders:
 
     self.test_raw = pd.read_csv(
       os.path.join(BASE_PATH, 'test.csv'),
+      usecols=['id','store_nbr', 'family', 'date', 'onpromotion'],
       dtype={
+          'id': 'uint32',
           'store_nbr': 'category',
           'family': 'category',
           'onpromotion': 'uint32',
@@ -56,6 +59,7 @@ class _Loaders:
         'cluster': 'category',
       },
     )
+    # self.store_info['date'] = self.store_info.date.dt.to_period('D')
 
     self.oil_info = pd.read_csv(
       os.path.join(BASE_PATH, 'oil.csv'),
@@ -65,6 +69,8 @@ class _Loaders:
       parse_dates=['date'],
     )
 
+    self.oil_info['date'] = self.oil_info.date.dt.to_period('D')
+
     self.transact = pd.read_csv(
       os.path.join(BASE_PATH, 'transactions.csv'),
       dtype={
@@ -72,6 +78,7 @@ class _Loaders:
       },
       parse_dates=['date'],
     )
+    self.transact['date'] = self.transact.date.dt.to_period('D')
 
     self.holidays = pd.read_csv(
       os.path.join(BASE_PATH, 'holidays_events.csv'),
@@ -83,6 +90,7 @@ class _Loaders:
       },
       parse_dates=['date'],
     )
+    self.holidays['date'] = self.holidays.date.dt.to_period('D')
 
 
     pickle.dump(self.train_raw, open(os.path.join(BASE_PATH, 'train.pkl'), 'wb'))
@@ -109,6 +117,59 @@ class _Loaders:
 
   def get_holidays(self):
     return self.holidays
+
+  def get_merged(self, train = True):
+    base = self.get_stores().reset_index().rename(columns={'type': 'store_type', 'cluster': 'store_cluster'})
+    
+    if train:
+      data = self.get_train().rename(columns={'id': 'class_id'}).reset_index()
+      print (data.dtypes)
+
+      train_cols = ["class_id", "family", "sales", "onpromotion"]
+      for c in train_cols:
+        base[c] = [None for i in range(0, base.shape[0])]
+
+      for i in range(0, data.shape[0]):
+        target_date = data.iloc[i].date
+        for c in train_cols:
+          item = data.iloc[i][c]
+          base.loc[(base.store_nbr == data.iloc[i].store_nbr) & (base.family == data.iloc[i].family), c] = item
+
+          
+
+    # merged = self.get_stores().reset_index().rename(columns={'type': 'store_type', 'cluster': 'store_cluster'})
+    #
+    #
+    # if train:
+    #   total_sales = self.get_train().reset_index().groupby(['store_nbr', 'date']).agg({'sales': 'sum', 'onpromotion': 'sum', 'family': 'count'}).reset_index()
+    #   print (total_sales.shape[0])
+    #   merged = merged.merge(total_sales, on='store_nbr', how='right', suffixes=('', '_ts'))
+    # else:
+    #   merged = merged.merge(self.get_test().reset_index().groupby(['store_nbr', 'date']).agg({'sales': 'sum', 'onpromotion': 'sum', 'family': 'count'}).reset_index(), on='store_nbr', how='left', suffixes=('', '_ts')).drop_columns('index_ts')
+    #
+    # holiday = self.get_holidays().reset_index().rename(columns={'type': 'holiday_type', 'locale': 'holiday_locale', 'locale_name': 'holiday_locale_name', 'description': 'holiday_description'})
+    # merged = merged.merge(holiday, on='date', how='left', suffixes=('', '_holiday'))
+    #
+    # transact = self.get_transactions().reset_index()
+    # merged = merged.merge(transact, on=('store_nbr', 'date'), how='left', suffixes=('', '_transact'))
+    #
+    #
+    # # merged['date'] = pd.to_datetime(merged['date'].astype(str), format='%Y-%m-%d') # TYPE : Conversion
+    #
+    # oil_prices = self.get_oil().reset_index().rename(columns={'dcoilwtico': 'oil_price'})
+    # merged = merged.merge(oil_prices, on='date', how='left', suffixes=('', '_oil'))
+    # merged['oil_price'] = merged['oil_price'].fillna(method='bfill')
+    #
+    #
+    # # RESULT : Reordering
+    # merged_columns: list[str] = merged.columns.values.tolist()
+    # for col in ['store_nbr', 'date', 'sales']:
+    #   merged_columns.remove(col)
+    # merged_columns = ['store_nbr', 'date'] + merged_columns + ['sales']
+    # merged = merged.reindex(columns=merged_columns)
+
+    return merged
+
 
 
 
