76dd57a302f94e5258712f57d17002d92aef1f0e
shellcodesniper <shellcodesniper@icloud.com>
2023-06-14 22:11:45 +0900

    Feat: Analyzer

--- analyze.py
+++ analyze.py
@@ -20,4 +20,61 @@ import dash_mantine_components as dmc
 
 # plot total average sale vs lags for train data (total -> mean of all stores and families)
 
-train_data = Loader.get_train()
+train_data: pd.DataFrame = Loader.get_train()
+store_info: pd.DataFrame = Loader.get_stores()
+oil_info: pd.DataFrame = Loader.get_oil()
+transact: pd.DataFrame = Loader.get_transactions()
+holidays: pd.DataFrame = Loader.get_holidays()
+
+plot_data = train_data.groupby(['date']).agg({'sales': 'sum'}).reset_index()
+plot_data['date'] = pd.to_datetime(plot_data['date'].astype(str), format='%Y-%m-%d')
+
+plot_data = plot_data.merge(oil_info, on='date', how='left')
+plot_data = plot_data.merge(transact, on='date', how='left')
+plot_data = plot_data.merge(store_info, on='store_nbr', how='left')
+
+fig = ms.make_subplots(rows=2, cols=2, subplot_titles=("Total Sales", "Oil Price", "Transactions", "Holidays"))
+
+fig.add_trace(
+  go.Scatter(x=plot_data['date'], y=plot_data['sales'], name='Total Sales'),
+  row=1, col=1
+)
+fig.add_trace(
+  go.Scatter(x=plot_data['date'], y=plot_data['dcoilwtico'], name='Oil Price'),
+  row=1, col=2
+)
+
+fig.add_trace(
+  go.Scatter(x=plot_data['date'], y=plot_data['transactions'], name='Transactions'),
+  row=2, col=1
+)
+
+fig.update_layout(height=600, width=800, title_text="Total Sales vs Oil Price vs Transactions vs Holidays")
+
+fig.show()
+
+mfig = ms.make_subplots(rows=4, cols=1, specs=[[{'rowspan':3}],[None],[None], [{'rowspan': 1}]], shared_xaxes=True, horizontal_spacing=0.03, vertical_spacing=0.01)
+
+# NOTE : Base Data
+mfig.add_trace(
+  go.Scatter(x=plot_data['date'], y=plot_data['sales'], name='Total Sales'),
+  row=1, col=1
+)
+
+# NOTE : Transaction Data
+mfig.add_trace(
+  go.Scatter(x=plot_data['date'], y=plot_data['transactions'], name='Transactions'),
+  row=4, col=1
+)
+
+mfig.update_layout(autosize=True, xaxis1_rangeslider_visible=False, xaxis2_rangeslider_visible=False, margin=dict(l=50, r=50, t=50, b=50), template='seaborn')
+  # dfig.update_layout(xaxis=dict(type="category", tickformat="%H%M"), xaxis2=dict(type="category", tickformat="%H%M"))
+mfig.update_xaxes(tickformat='%Y.%m.%d', zeroline=True, zerolinewidth=1, zerolinecolor='black', showgrid=True, gridwidth=1, gridcolor='lightgray', showline=True, linewidth=1, linecolor='black', mirror=True)
+mfig.update_yaxes(tickformat=',d', zeroline=True, zerolinewidth=1, zerolinecolor='black', showgrid=True, gridwidth=2, gridcolor='lightgray',showline=True,linewidth=2, linecolor='black', mirror=True)
+mfig.update_traces(xhoverformat='%Y.%m.%d') 
+mfig.update_layout(width=1920, height=800)
+mfig.update_layout(paper_bgcolor='white', plot_bgcolor='white')
+mfig.update_layout(hovermode="x unified")
+
+mfig.show()
+
--- kuuwange/loaders.py
+++ kuuwange/loaders.py
@@ -1,8 +1,24 @@
-import os, glob, tqdm, numpy as np, pandas as pd
-BASE_PATH = 'data/'
+import os, glob, tqdm, pickle
+import numpy as np, pandas as pd
+BASE_PATH = 'datas/'
 
 class _Loaders:
   def __init__(self):
+    if (
+      os.path.exists(os.path.join(BASE_PATH, 'train.pkl'))
+      and os.path.exists(os.path.join(BASE_PATH, 'test.pkl'))
+      and os.path.exists(os.path.join(BASE_PATH, 'stores.pkl'))
+      and os.path.exists(os.path.join(BASE_PATH, 'oil.pkl'))
+      and os.path.exists(os.path.join(BASE_PATH, 'transactions.pkl'))
+      and os.path.exists(os.path.join(BASE_PATH, 'holidays.pkl'))
+    ):
+      self.train_raw = pickle.load(open(os.path.join(BASE_PATH, 'train.pkl'), 'rb'))
+      self.test_raw = pickle.load(open(os.path.join(BASE_PATH, 'test.pkl'), 'rb'))
+      self.store_info = pickle.load(open(os.path.join(BASE_PATH, 'stores.pkl'), 'rb'))
+      self.oil_info = pickle.load(open(os.path.join(BASE_PATH, 'oil.pkl'), 'rb'))
+      self.transact = pickle.load(open(os.path.join(BASE_PATH, 'transactions.pkl'), 'rb'))
+      self.holidays = pickle.load(open(os.path.join(BASE_PATH, 'holidays.pkl'), 'rb'))
+      return
     self.train_raw = pd.read_csv(
     os.path.join(BASE_PATH, 'train.csv'),
       usecols=['store_nbr', 'family', 'date', 'sales', 'onpromotion'],
@@ -13,7 +29,6 @@ class _Loaders:
         'onpromotion': 'uint32',
       },
       parse_dates=['date'],
-      infer_datetime_format=True,
     )
     self.train_raw['date'] = self.train_raw.date.dt.to_period('D')
     self.train_raw = self.train_raw.set_index(['store_nbr', 'family', 'date']).sort_index()
@@ -26,16 +41,74 @@ class _Loaders:
           'onpromotion': 'uint32',
       },
       parse_dates=['date'],
-      infer_datetime_format=True,
     )
     self.test_raw['date'] = self.test_raw.date.dt.to_period('D')
     self.test_raw = self.test_raw.set_index(['store_nbr', 'family', 'date']).sort_index()
+
+    self.store_info = pd.read_csv(
+      os.path.join(BASE_PATH, 'stores.csv'),
+      usecols=['store_nbr', 'city', 'state', 'type', 'cluster'],
+      dtype={
+        'store_nbr': 'category',
+        'city': 'category',
+        'state': 'category',
+        'type': 'category',
+        'cluster': 'category',
+      },
+    )
+
+    self.oil_info = pd.read_csv(
+      os.path.join(BASE_PATH, 'oil.csv'),
+      dtype={
+        'dcoilwtico': 'float32',
+      },
+      parse_dates=['date'],
+    )
+
+    self.transact = pd.read_csv(
+      os.path.join(BASE_PATH, 'transactions.csv'),
+      dtype={
+        'store_nbr': 'category',
+      },
+      parse_dates=['date'],
+    )
+
+    self.holidays = pd.read_csv(
+      os.path.join(BASE_PATH, 'holidays_events.csv'),
+      dtype={
+        'type': 'category',
+        'locale': 'category',
+        'locale_name': 'category',
+        'description': 'category',
+      },
+      parse_dates=['date'],
+    )
+
+
+    pickle.dump(self.train_raw, open(os.path.join(BASE_PATH, 'train.pkl'), 'wb'))
+    pickle.dump(self.test_raw, open(os.path.join(BASE_PATH, 'test.pkl'), 'wb'))
+    pickle.dump(self.store_info, open(os.path.join(BASE_PATH, 'stores.pkl'), 'wb'))
+    pickle.dump(self.oil_info, open(os.path.join(BASE_PATH, 'oil.pkl'), 'wb'))
+    pickle.dump(self.transact, open(os.path.join(BASE_PATH, 'transactions.pkl'), 'wb'))
+    pickle.dump(self.holidays, open(os.path.join(BASE_PATH, 'holidays.pkl'), 'wb'))
 
   def get_train(self):
     return self.train_raw
   
   def get_test(self):
     return self.test_raw
+
+  def get_stores(self):
+    return self.store_info
+
+  def get_oil(self):
+    return self.oil_info
+
+  def get_transactions(self):
+    return self.transact
+
+  def get_holidays(self):
+    return self.holidays
 
 
 
